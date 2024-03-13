27817d2fefd759572b0acab864714d0b58cad211
Kheoss <kheoss@localhost.localdomain>
2024-03-13 12:14:56 +0200

    Add ICPM analysis

--- probing/ittl_probe.py
+++ probing/ittl_probe.py
@@ -1,4 +1,13 @@
 from scapy.all import *
+import collections
+from enum import Enum, auto
+
+class SequenceNumberBehavior(Enum):
+    INCREMENTAL = auto()
+    RANDOM = auto()
+    ZERO = auto()
+    DUPLICATES = auto()
+    UNDETERMINED = auto()
 
 class ITTLProbe:
 
@@ -17,19 +26,56 @@ class ITTLProbe:
 
 
     @staticmethod
-    def get_icmp_ittl_probe(ip, port=33533):
-        packet = IP(dst=ip)/ICMP()
-        
-        response = sr1(packet, timeout=1, verbose=0)
-        
-        if response is None:
-            print(f"No response from {ip}")
-        else:
-            # Retrieve and print the TTL value from the response
-            ttl = response[IP].ttl
-            print(f"Response from {ip} with TTL: {ttl}")
+    def get_icmp_ittl_probe(target_ip, count=3):
+        ttl_values = []
+        sequence_numbers = []
+        response_sizes = []
+        for i in range(count):
+            packet = IP(dst=target_ip)/ICMP(seq=i)
+            
+            response = sr1(packet, timeout=1, verbose=0)
+            
+            if response is not None:
+                ttl = response[IP].ttl
+                seq = response[ICMP].seq
+                size = len(response)
+                ttl_values.append(ttl)
+                sequence_numbers.append(seq)
+                response_sizes.append(size)
+                print(f"Response from {target_ip}: TTL = {ttl}, Seq = {seq}, Size: {size}")
+            else:
+                print(f"No response for packet {i}")
 
-ITTLProbe.get_icmp_ittl_probe('193.239.116.205')
+        return ttl_values, sequence_numbers, response_sizes
+
+    @staticmethod
+    def analyze_sequence_numbers(sequence_numbers):
+        if not sequence_numbers:
+            print("No sequence numbers to analyze.")
+            return SequenceNumberBehavior.UNDETERMINED
+        
+        counter = collections.Counter(sequence_numbers)
+        if len(counter) == 1 and sequence_numbers[0] == 0:
+            print("All sequence numbers are zero.")
+            return SequenceNumberBehavior.ZERO
+        elif len(counter) == len(sequence_numbers):
+            if all(x < y for x, y in zip(sequence_numbers, sequence_numbers[1:])):
+                print("Sequence numbers are incremental.")
+                return SequenceNumberBehavior.INCREMENTAL
+            else:
+                print("Sequence numbers are random.")
+                return SequenceNumberBehavior.RANDOM
+        else:
+            print("Sequence numbers contain duplicates.")
+        
+            return SequenceNumberBehavior.DUPLICATES
+        return SequenceNumberBehavior.UNDETERMINED
+
+ttl_values, sequence_numbers, response_sizes = ITTLProbe.get_icmp_ittl_probe('193.239.116.205')
+ITTLProbe.analyze_sequence_numbers(sequence_numbers)
+
+
+# ITTLProbe.get_icmp_ittl_probe()
 
 # Example usage
 
