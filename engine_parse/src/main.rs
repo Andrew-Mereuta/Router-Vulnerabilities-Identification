dc96a79e90007e49b2a12b7801fa6bf0a7b9022c
Csanad Farkas <farkas.csacsa@gmail.com>
2024-03-15 14:23:14 +0100

    add working version

--- engine_parse/src/main.rs
+++ engine_parse/src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{Ok, Result};
+use anyhow::Result;
 use csv::ReaderBuilder;
 use std::{fs, u128};
 use thiserror::Error;
@@ -49,27 +49,71 @@ enum DeviceParseError {
 #[derive(Debug, PartialEq, Eq)]
 enum DeviceID<'a> {
     Mac {
-        address: u128,
-        blocks: &'a [MacBlock],
+        address: String,
+        blocks: Vec<&'a MacBlock>,
     },
-    IPv4(u128),
+    IPv4([u8; 4]),
     IPv6(u128),
-    Text(u128),
-    Octid(u128),
+    Text(String),
+    Octid([u8; 27]),
 }
 
-impl DeviceID<'_> {
-    fn new(id: u128, overflow: u128, mac_blocks: &[MacBlock]) -> Result<Self> {
-        let format_mask = 0x0000_0000_ff00_0000_0000_0000;
-        let id_mask = 0x0000_0000_00ff_ffff_ffff_ffff;
-        Ok(match ((id & format_mask) >> 56) as u8 {
-            1 => DeviceID::IPv4(id_mask & id),
-            2 => DeviceID::IPv6(id_mask & id),
+impl<'a> DeviceID<'a> {
+    fn new(id: u128, overflow: u128, id_l: usize, mac_blocks: &'a [MacBlock]) -> Result<Self> {
+        let format_mask = 0x0000_0000_ff00_0000_0000_0000_0000_0000;
+        let id_mask = 0x0000_0000_00ff_ffff_ffff_ffff_ffff_ffff;
+        println!("-- device step --");
+        println!(
+            "{}",
+            format!("{:#034x}{:#034x}, {}", id, overflow, id_l).replace("0x", "")
+        );
+        println!(
+            "{}",
+            format!(
+                "--format:--\n{:#034x}\n{:#034x}\n{:#034x}",
+                format_mask,
+                id & format_mask,
+                (id & format_mask) >> 88
+            )
+            .replace("0x", "")
+        );
+        println!(
+            "{}",
+            format!(
+                "--id:--\n{:#034x}\n{:#034x}\n{:#034x}",
+                id_mask,
+                id & id_mask,
+                (id & id_mask) >> 32,
+            )
+            .replace("0x", "")
+        );
+        Ok(match ((id & format_mask) >> 88) as u8 {
+            1 => DeviceID::IPv4((((id_mask & id) >> 56) as u32).to_be_bytes()),
+            2 => DeviceID::IPv6(((id_mask & id) << 40) + (overflow >> 88)),
             3 => {
-                let id_str = 0;
-                let macs = mac_blocks.iter().filter(|mb| unimplemented!("oops!"));
-                unimplemented!("oh oh!")
+                let id_str = (id_mask & id).to_be_bytes()[6..12]
+                    .iter()
+                    .map(|b| format!("{:#04x}", b)[2..].to_owned())
+                    .collect::<Vec<_>>()
+                    .join(":");
+                println!("parsed mac: {}", id_str);
+                let macs = mac_blocks
+                    .iter()
+                    .filter(|mb| id_str.starts_with(&mb.prefix.to_lowercase()))
+                    .collect();
+                DeviceID::Mac {
+                    address: id_str,
+                    blocks: macs,
+                }
             }
+            4 | 128..=255 => DeviceID::Text(
+                format!("{:#x}{:#034x}", id_mask & id, overflow).replace("0x", "")[..id_l - 10]
+                    .to_string(),
+            ),
+            5 => DeviceID::Octid(
+                [&(id_mask & id).to_be_bytes()[5..], &overflow.to_be_bytes()].concat()[..28]
+                    .try_into()?,
+            ),
             n => Err(DeviceParseError::UnusedFormat(id, n))?,
         })
     }
@@ -97,9 +141,15 @@ impl<'a> EngineID<'a> {
     fn new(
         id: u128,
         overflow: u128,
+        id_l: usize,
         vendors: &'a [Vendor],
         mac_blocks: &'a [MacBlock],
     ) -> Result<Self> {
+        println!("-- engine step --");
+        println!(
+            "{}",
+            format!("{:#034x}{:#034x}", id, overflow).replace("0x", "")
+        );
         let conform_mask = 0x8000_0000_0000_0000_0000_0000_0000_0000;
         let vendor_mask = 0x7fff_ffff_0000_0000_0000_0000_0000_0000;
         let vendor = &vendors[((vendor_mask & id) >> 96) as usize];
@@ -112,7 +162,7 @@ impl<'a> EngineID<'a> {
         } else {
             Ok(EngineID::CorrectFormat {
                 vendor,
-                id: DeviceID::new(id, overflow, mac_blocks)?,
+                id: DeviceID::new(id, overflow, id_l, mac_blocks)?,
             })
         }
     }
@@ -126,9 +176,11 @@ impl<'a> EngineID<'a> {
             Err(EngineParseError::IdTooLong(id_str.to_owned(), id_str.len()))?
         }
         let padded_str = format!("{:0<64}", id_str);
+        println!("-- string step --");
+        println!("{}\n{}", id_str, padded_str);
         let id = u128::from_str_radix(&padded_str[..32], 16)?;
         let overflow = u128::from_str_radix(&padded_str[32..], 16)?;
-        EngineID::new(id, overflow, vendors, mac_blocks)
+        EngineID::new(id, overflow, id_str.len(), vendors, mac_blocks)
     }
 }
 
@@ -212,13 +264,44 @@ fn load_mac_blocks(file: &str) -> Result<Vec<MacBlock>> {
 }
 
 fn main() -> Result<()> {
+    // TODO: move file names into parameters?
     let file =
         fs::read_to_string("data/enterprise-numbers").expect("Missing the enterprise numbers file");
-    let _ = load_vendors(&file)?;
+    let vendors = load_vendors(&file)?;
     let file = fs::read_to_string("data/mac-vendors-export.json")
         .expect("Missing the file for the MAC blocks.");
-    let _ = load_mac_blocks(&file)?;
+    let mac_blocks = load_mac_blocks(&file)?;
     println!("Files loaded, loading data...\n");
+    let ids = ReaderBuilder::new()
+        .from_reader(
+            fs::read_to_string("data/snmp_results.csv")
+                .unwrap_or_else(|_| {
+                    panic!("No SNMP data file found at `{}`", "data/snmp_results.csv")
+                })
+                .as_bytes(),
+        )
+        .records()
+        .filter_map(|res| {
+            // TODO: discuss error handling and fail conditions
+            let rec = match res {
+                Err(error) => {
+                    println!("Something went wrong while parsing.({})", error);
+                    return None;
+                }
+                Ok(val) => val,
+            };
+            rec.get(4).map(|entry| match entry {
+                "Error" => None,
+                id_str => match EngineID::from_string(id_str, &vendors, &mac_blocks) {
+                    Err(error) => {
+                        println!("Error parsing id({}): {}", id_str.to_owned(), error);
+                        None
+                    }
+                    eng_id => Some((id_str.to_owned(), eng_id)),
+                },
+            })
+        });
+    // TODO: write out ids to another CSV file
     Ok(())
 }
 
@@ -249,7 +332,7 @@ mod tests {
         let mac_blocks = load_mac_blocks(&file);
         match mac_blocks {
             Err(error) => panic!("Got an error: {}", error),
-            Ok(list) => (),
+            Ok(_list) => (),
         }
     }
 
@@ -265,6 +348,7 @@ mod tests {
         let custom = EngineID::new(
             0x0000_0009_abaf_aaaa_1234_5678_0000_0000,
             0,
+            24,
             &vendors,
             &mac_blocks,
         )
@@ -284,6 +368,53 @@ mod tests {
             EngineID::CustomFormat {
                 vendor: &vendors[9],
                 id: 0xabaf_aaaa_1234_5678
+            }
+        );
+
+        let custom = EngineID::from_string("8000000901c0a80165", &vendors, &mac_blocks)
+            .expect("Correct ID should parse (from str).");
+        assert_eq!(
+            custom,
+            EngineID::CorrectFormat {
+                vendor: &vendors[9],
+                id: crate::DeviceID::IPv4([0xc0, 0xa8, 0x01, 0x65]),
+            }
+        );
+
+        let custom = EngineID::from_string(
+            "800000090200112233445566778899aabbccddeeff",
+            &vendors,
+            &mac_blocks,
+        )
+        .expect("Correct ID should parse (from str).");
+        assert_eq!(
+            custom,
+            EngineID::CorrectFormat {
+                vendor: &vendors[9],
+                id: crate::DeviceID::IPv6(0x00112233445566778899aabbccddeeff),
+            }
+        );
+
+        let custom = EngineID::from_string("8000000904afaaaa12345678", &vendors, &mac_blocks)
+            .expect("Correct ID should parse (from str).");
+        assert_eq!(
+            custom,
+            EngineID::CorrectFormat {
+                vendor: &vendors[9],
+                id: crate::DeviceID::Text("afaaaa12345678".to_string())
+            }
+        );
+
+        let custom = EngineID::from_string("80000009030020cfae95c1a0", &vendors, &mac_blocks)
+            .expect("Correct ID should parse (from str).");
+        assert_eq!(
+            custom,
+            EngineID::CorrectFormat {
+                vendor: &vendors[9],
+                id: crate::DeviceID::Mac {
+                    address: "20:cf:ae:95:c1:a0".to_owned(),
+                    blocks: vec![&mac_blocks[42160]],
+                }
             }
         );
     }
